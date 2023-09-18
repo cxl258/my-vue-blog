742a7f18d5b3fac8d5c72c25ed77e64bc443e5f6
cxl <3288270459@qq.com>
2023-09-18 22:32:29 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+</script>
+<template>
+  <div class="nav">
+    <div class="top">
+      <!-- 导航栏 -->
+       <div><a>首页</a></div>
+       <div><a>文章</a></div>
+       <div><a>相册</a></div>
+       <div><a>api文档</a></div>
+       <div><a>关于</a></div>
+    </div>
+    <!-- 内容切换 -->
+    <div>
+      <!-- <router-view></router-view> -->
+    </div>
+  </div>
+  <!-- 备案信息 -->
+    <div class="baxx">
+      <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#fff;">冀ICP备2023018480号</p><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13012102000115" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/image/tu.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#fff;">冀公网安备 13012102000115号</p></a>
+    </div>
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  color: black;
+}
+.nav{
+  width: 90vw;
+  height: 80vh;
+  background-color: #fff;
+  opacity: .8;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 1vw;
+}
+.top{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 5vh;
+  border-bottom: 1px solid rgb(136, 136, 136);
+}
+.baxx {
+    position: fixed;
+    width: auto;
+    margin: auto;
+    bottom: 2%;
+    left: 50%;
+    transform: translate(-50%);
+    font-size: 16px;
+}
+</style>
---
+++ src/views/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {};
+  },
+};
+</script>
+<style scoped></style>
